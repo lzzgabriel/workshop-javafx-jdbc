d289187434c0f8cc6785b6dc7f5cacf2bd94cdf6
lzzgabriel <gabrielhlezo@gmail.com>
2024-03-01 16:54:36 -0300

    MainView design

--- src/main/java/dev/lzzgabriel/workshop/App.java
+++ src/main/java/dev/lzzgabriel/workshop/App.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,27 +14,32 @@ import java.io.IOException;
  */
 public class App extends Application {
 
-    private static Scene scene;
+  private static Scene scene;
 
-    @Override
-    public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("MainView"), 640, 480);
-        stage.setScene(scene);
-        stage.setTitle("Sample JavaFX App");
-        stage.show();
-    }
+  @Override
+  public void start(Stage stage) throws IOException {
+    var scrollPane = (ScrollPane) loadFXML("MainView");
+    
+    scrollPane.setFitToHeight(true);
+    scrollPane.setFitToWidth(true);
+    
+    scene = new Scene(scrollPane, 640, 480);
+    stage.setScene(scene);
+    stage.setTitle("Sample JavaFX App");
+    stage.show();
+  }
 
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
+  static void setRoot(String fxml) throws IOException {
+    scene.setRoot(loadFXML(fxml));
+  }
 
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
-    }
+  private static Parent loadFXML(String fxml) throws IOException {
+    FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+    return fxmlLoader.load();
+  }
 
-    public static void main(String[] args) {
-        launch();
-    }
+  public static void main(String[] args) {
+    launch();
+  }
 
 }
