f5614f6c128fdddaacbad025dba99170f761abae
lzzgabriel <gabrielhlezo@gmail.com>
2024-03-04 08:42:12 -0300

    Department list view design

--- src/main/java/dev/lzzgabriel/workshop/controller/MainViewController.java
+++ src/main/java/dev/lzzgabriel/workshop/controller/MainViewController.java
@@ -29,7 +29,7 @@ public class MainViewController implements Initializable {
   
   @FXML
   public void onMenuItemDepartmentAction() {
-    System.out.println("onMenuItemDepartmentAction");
+    loadView("DepartmentList");
   }
   
   @FXML
