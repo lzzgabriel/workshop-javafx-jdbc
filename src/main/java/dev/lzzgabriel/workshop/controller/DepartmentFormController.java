3ddac789e8f24c786d0901e3846468cf365c16c4
lzzgabriel <gabrielhlezo@gmail.com>
2024-03-05 14:00:18 -0300

    Saving a new department

--- src/main/java/dev/lzzgabriel/workshop/controller/DepartmentFormController.java
+++ src/main/java/dev/lzzgabriel/workshop/controller/DepartmentFormController.java
@@ -3,10 +3,16 @@ package dev.lzzgabriel.workshop.controller;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import dev.lzzgabriel.workshop.db.DbException;
 import dev.lzzgabriel.workshop.model.entities.Department;
+import dev.lzzgabriel.workshop.model.services.DepartmentService;
+import dev.lzzgabriel.workshop.util.Alerts;
 import dev.lzzgabriel.workshop.util.Constraints;
+import dev.lzzgabriel.workshop.util.Utils;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -30,14 +36,25 @@ public class DepartmentFormController implements Initializable {
   
   private Department entity;
   
+  private DepartmentService service;
+  
   @FXML
-  public void onBtnSaveAction() {
-    
+  public void onBtnSaveAction(ActionEvent event) {
+    entity = getFormData();
+    if (service == null) {
+      throw new IllegalStateException("DepartmentService not available: not set");
+    }
+    try {
+      service.saveOrUpdate(entity);
+    } catch (DbException e) {
+      Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
+      Utils.currentStage(event).close();
+    }
   }
   
   @FXML
-  public void onBtnCancelAction() {
-    
+  public void onBtnCancelAction(ActionEvent event) {
+    Utils.currentStage(event).close();
   }
 
   @Override
@@ -56,9 +73,22 @@ public class DepartmentFormController implements Initializable {
     txtID.setText(String.valueOf(entity.getId()));
     txtName.setText(entity.getName());
   }
+  
+  public Department getFormData() {
+    var obj = new Department();
+    
+    obj.setId(Utils.tryParseToInt(txtID.getText()));
+    obj.setName(txtName.getText());
+    
+    return obj;
+  }
 
   public void setDepartment(Department entity) {
     this.entity = entity;
+  }
+
+  public void setService(DepartmentService service) {
+    this.service = service;
   }
 
 }
--- src/main/java/dev/lzzgabriel/workshop/util/Utils.java
+++ src/main/java/dev/lzzgabriel/workshop/util/Utils.java
@@ -9,5 +9,13 @@ public class Utils {
   public static Stage currentStage(ActionEvent e) {
     return (Stage) ((Node) e.getSource()).getScene().getWindow();
   }
+  
+  public static Integer tryParseToInt(String str) {
+    try {
+      return Integer.parseInt(str);
+    } catch (NumberFormatException e) {
+      return null;
+    }
+  }
 
 }
